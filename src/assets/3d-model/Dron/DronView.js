da4515155067a11900a65837783958031ea2ece7
Ricardo Hurtado <ricardo040400@gmail.com>
2023-07-12 01:26:31 -0400

    first commit

---
+++ src/assets/3d-model/Dron/DronView.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+function DronView() {
+  return (
+    <article id="main" class="content">
+      <section id="showcase">
+        <div id="html5-showcase" class="showcase">
+          <div id="threesixty" class="container">
+            <section id="threesixty-main">
+              <div id="viewer"></div>
+            </section>
+          </div>
+        </div>
+      </section>
+    </article>
+  );
+}
+
+export default DronView;
