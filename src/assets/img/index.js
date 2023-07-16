0b5737b948dd4695a16bbbad39a7b7a50f3143ea
Ricardo Hurtado <ricardo040400@gmail.com>
2023-07-16 19:20:41 -0400

    Assets (img) para dron actualizados

--- src/assets/img/index.js
+++ src/assets/img/index.js
@@ -10,6 +10,12 @@ const tryRequire = (path) => {
 const imageExports = {
   dron: tryRequire("./Dron.png") || require("./Sombra.png"),
   dronPrueba: tryRequire("./Roughness.png") || require("./Sombra.png"),
+  dronRoja: tryRequire("./dron-red.jpg") || require("./Sombra.png"),
+  dronExplode: tryRequire("./dron-explode.png") || require("./Sombra.png"),
+  dronEstructura: tryRequire("./estructura.png") || require("./Sombra.png"),
+  camaraAire: tryRequire("./camara-aire.png") || require("./Sombra.png"),
+  motor: tryRequire("./motor-brush.png") || require("./Sombra.png"),
+  imgLarge: tryRequire("./large-image.png") || require("./Sombra.png"),
 };
 
 export default imageExports;
--- src/assets/lists/list.js
+++ src/assets/lists/list.js
@@ -2,7 +2,7 @@ import images from "../img/index.js";
 
 const portfoliolist = [
   {
-    path: images.dronPrueba,
+    path: images.dronRoja,
     alt: "dron sombra",
     color: "#f5f5f5",
     link: "/dron-model",
@@ -12,21 +12,13 @@ const portfoliolist = [
         path: images.dron,
       },
       {
-        path: images.dron,
-      },
-    ],
-  },
-  {
-    path: images.dronPrueba,
-    alt: "dron sombra",
-    color: "#f5f5f5",
-    link: "/dron-model",
-    imagesSlide: [
-      {
-        path: images.dron,
+        path: images.dronExplode,
       },
       {
-        path: images.dron,
+        path: images.dronEstructura,
+      },
+      {
+        path: images.camaraAire,
       },
     ],
   },
