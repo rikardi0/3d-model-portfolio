f10cfa7f44fb01032cb6118b0b0cb2cc1b263967
Ricardo Hurtado <ricardo040400@gmail.com>
2023-08-24 01:05:27 -0400

    Ajuste de estilos en clase Details

--- src/assets/ui/detail/Details.js
+++ src/assets/ui/detail/Details.js
@@ -1,70 +1,42 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Menu from "../menu/menu.js";
 import Contact from "../../../components/footer/Footer.js";
-import image from "../../../assets/img/dron/index.js";
 import "./Details.css";
 
-class Details extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      element: props.element,
-      description: props.description,
-      direction: props.direction,
-    };
-  }
+function Details(props) {
+  const [element] = useState(props.element);
+  const [description] = useState(props.description);
+  const [imageLarge] = useState(props.imageLarge);
+  const [imageList] = useState(props.imageList);
 
-  render() {
-    let direct = this.state.direction;
-    return (
-      <>
-        <div id="black-spacer">
-          <Menu />
+  return (
+    <>
+      <div id="black-spacer">
+        <Menu />
+      </div>
+      <div>{element}</div>
+      <div id="orange-spacer"></div>
+      <div id="description-text">
+        <div>
+          <img id="large-img" src={imageLarge} alt="images"></img>
+          <div className="description-content">{description}</div>
         </div>
-        <div>{this.state.element}</div>
-
-        <div id="white-spacer"></div>
-        <div id="orange-spacer"></div>
-
-        <div id="description-text">
-          {this.state.description.map(function (element, index) {
+        <div id="container-images">
+          {imageList.map(function (images, index) {
             return (
-              <div key={index}>
-                <div id="img-description" style={{ flexDirection: direct }}>
-                  <img
-                    className="img-column"
-                    src={image.motor}
-                    alt="images"
-                  ></img>
-                  <div>
-                    <div id="introduction" className="description-content">
-                      {element.introduction}
-                    </div>{" "}
-                    <div id="introduction" className="description-content">
-                      {element.introductionSecond}
-                    </div>
-                  </div>
-                </div>
-                <div id="closer">
-                  <div className="description-content">
-                    {element.description}
-                  </div>{" "}
-                  <div className="description-content">
-                    {element.descriptionSecond}
-                  </div>{" "}
-                  <img id="large-img" src={image.imgLarge} alt="images"></img>
-                  <div className="description-content">
-                    {element.descriptionThird}
-                  </div>
-                </div>
-              </div>
+              <div
+                className="img-element"
+                style={{
+                  backgroundImage: `url("${images}")`,
+                }}
+              ></div>
             );
           })}
         </div>
-        <Contact />
-      </>
-    );
-  }
+      </div>
+      <Contact />
+    </>
+  );
 }
 
 export default Details;
