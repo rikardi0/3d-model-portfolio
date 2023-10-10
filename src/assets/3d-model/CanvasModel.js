2e2c1c17d82f5eefadcaf6880e5f1a9d3dcda875
Ricardo Hurtado <ricardo040400@gmail.com>
2023-10-10 14:44:30 -0400

    Loader screen agregada

--- src/assets/3d-model/CanvasModel.js
+++ src/assets/3d-model/CanvasModel.js
@@ -1,27 +1,9 @@
-import React, { Suspense, useState, useEffect } from "react";
+import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Loader, OrbitControls } from "@react-three/drei";
 import "./CanvasModel.css";
 
 function CanvasModel(props) {
-  const transitionTime = 1000;
-
-  const [isTransitionComplete, setTransitionComplete] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setTransitionComplete(true);
-    }, transitionTime);
-
-    // Clean up the timer when the component unmounts
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
-
-  // Determine the background color based on whether the transition is complete
-  const backgroundColor = isTransitionComplete ? "#FFFFFF" : "#FFFFFF";
-
   return (
     <section id="container">
       <div id="container-triangle-left">
@@ -29,50 +11,33 @@ function CanvasModel(props) {
         <div className="triangle-background" id="orange-background"></div>
       </div>
       <div id="canvas-container">
-        {isTransitionComplete ? (
-          <div id="canvas-container">
-            <Canvas
-              camera={{
-                zoom: props.zoom,
-                position: [props.x, props.y, props.z],
-                fov: 60,
-              }}
-            >
-              <ambientLight intensity={0.5} />
-              <pointLight
-                position={[0, 1.5, 2]}
-                intensity={20}
-                color={"white"}
-              />
-              <pointLight position={[2, 0, 0]} intensity={2} color={"white"} />
-              <pointLight
-                position={[0, 0, 2]}
-                intensity={5}
-                color={"#F3AB5E "}
-              />
-              <pointLight
-                position={[0, 0, -1]}
-                intensity={1}
-                color={"#1c3833"}
-              />
+        <div id="canvas-container">
+          <Canvas
+            camera={{
+              zoom: props.zoom,
+              position: [props.x, props.y, props.z],
+              fov: 60,
+            }}
+          >
+            <ambientLight intensity={0.5} />
+            <pointLight position={[0, 1.5, 2]} intensity={20} color={"white"} />
+            <pointLight position={[2, 0, 0]} intensity={2} color={"white"} />
+            <pointLight position={[0, 0, 2]} intensity={5} color={"#F3AB5E "} />
+            <pointLight position={[0, 0, -1]} intensity={1} color={"#1c3833"} />
 
-              <Suspense fallback={null}>
-                <group position={[0, props.verticalAdjustment, 0]}>
-                  {props.modelo}
-                </group>
-              </Suspense>
-              <OrbitControls
-                enablePan={true}
-                panSpeed={0.00005}
-                autoRotate={true}
-              />
-            </Canvas>
-          </div>
-        ) : (
-          <div id="loading-screen">
-            <div class="spinner"></div>
-          </div>
-        )}
+            <Suspense fallback={null}>
+              <group position={[0, props.verticalAdjustment, 0]}>
+                {props.modelo}
+              </group>
+            </Suspense>
+            <OrbitControls
+              enablePan={true}
+              panSpeed={0.00005}
+              autoRotate={true}
+            />
+          </Canvas>
+          <Loader />
+        </div>
       </div>
       <div id="container-triangle-right">
         <div className="triangle-background" id="black-background"></div>
